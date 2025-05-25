ddb330686c351b3b7f044b7bae566c07c6c84b8f
Amarjit Singh <118879833+amarjit03@users.noreply.github.com>
2025-05-25 15:40:59 +0530

    feat: add account update, avatar, and cover image functionality to user controller; update exports

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@ import uploadCloudinary from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
 import { response } from "express";
+import { use } from "react";
 
 const generateAccessAndRefreshTokens = async(userId) => {
   try {
@@ -257,12 +258,82 @@ const getCurrentUser = asyncHandler(async(req,res) => {
 })
 
 
+const updateAccountDetails = asyncHandler(async(req,res) => {
+  const {full,name} = req.body
+  
+  if(!fullname || !email){
+    throw new ApiError(400,"All field are required")
+  }
+
+  const user  = User.findByIdAndUpdate(req.user?._id,
+    {
+      $set:{
+        fullname:fullname,
+        email:email,
+      }
+
+    },
+    {user:true}
+  ).select("-password")
+
+  return res.
+  status(200)
+  .json(new ApiResponse(200,user,"Account details updated sucessfully"))
+
+})
+
+const updateUserAvatar = asyncHandler(async(req,res) => {
+  const avataLocalPath = req.file?.path
+  if(!avataLocalPath){
+    throw new ApiError(400,"unable to find avatar")
+  }
+
+  const avatar = await uploadCloudinary(avataLocalPath)
+
+  if (!avatar.url){
+    throw new ApiError(400,"Error while  uploading avatar")
+  }
+  await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set:{
+        avatar:avatar.url
+      }
+    },
+    {new:true}
+  ).select("-password")
+})
+
+const updateUserCoverImage = asyncHandler(async(req,res) => {
+  const coverImageLocalPath = req.file?.path
+  if(!coverImageLocalPath){
+    throw new ApiError(400,"unable to find coverImage")
+  }
+
+  const coverImage = await uploadCloudinary(coverImageLocalPath)
+
+  if (!coverImage.url){
+    throw new ApiError(400,"Error while  uploading coverImage")
+  }
+  await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set:{
+        coverImage:coverImage.url
+      }
+    },
+    {new:true}
+  ).select("-password")
+})
 
 export { registerUser,
          loginUser,
          logoutUser,
          refreshAccessToken,
          changeCurrentPassword,
-         getCurrentUser
+         getCurrentUser,
+         updateAccountDetails,
+         updateUserAvatar,
+         updateUserCoverImage
 
 };
