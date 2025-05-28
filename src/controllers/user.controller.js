62abe26af4cc1807f7dd9557a962389c829f51a2
Amarjit Singh <118879833+amarjit03@users.noreply.github.com>
2025-05-28 11:30:59 +0530

    feat: enhance user channel profile retrieval and add watch history functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@ import uploadCloudinary from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
 import { response } from "express";
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshTokens = async(userId) => {
   try {
@@ -339,56 +340,62 @@ const getUserChannelProfile = asyncHandler(async(req,res) => {
   }
 
   const channel = await User.aggregate([
-    {
-      $match: {
+  {
+    $match: {
       username: username?.toLowerCase()
-     }
-    },
-    {
-      $lookup: {
+    }
+  },
+  {
+    $lookup: {
       from: "subscriptions",
       localField: "_id",
       foreignField: "channel",
       as: "subscribers"
-      }
-    },
-    {
-      $lookup: {
+    }
+  },
+  {
+    $lookup: {
       from: "subscriptions",
       localField: "_id",
       foreignField: "subscriber",
       as: "subscribedTo"
-      }
-    },
-    {
-      $addFields:{
-        subscriberCount: {
-          $size: "$subscribers"
-        },
-        channelSubscribedToCount:{
-          $size: "$subscribedTo"
-
-        },
-        $cond:{
-          if: {$in: [req.user?._id,"$subscribers"]},
+    }
+  },
+  {
+    $addFields: {
+      subscriberCount: { $size: "$subscribers" },
+      channelSubscribedToCount: { $size: "$subscribedTo" },
+      isSubscribed: {
+        $cond: {
+          if: {
+            $in: [req.user?._id, {
+              $map: {
+                input: "$subscribers",
+                as: "s",
+                in: "$$s.subscriber"
+              }
+            }]
+          },
           then: true,
           else: false
         }
       }
-    },
-    {
-      $project: {
-        fullname: 1,
-        username: 1,
-        subscriberCount: 1,
-        channelSubscribedToCount:1,
-        isSubscribed:1,
-        avatar:1,
-        coverImage:1,
-        email:1
-      }
     }
-  ])
+  },
+  {
+    $project: {
+      fullname: 1,
+      username: 1,
+      subscriberCount: 1,
+      channelSubscribedToCount: 1,
+      isSubscribed: 1,
+      avatar: 1,
+      coverImage: 1,
+      email: 1
+    }
+  }
+]);
+
   if (!channel?.length){
     throw new ApiError(400,"unable to get channel data")
   }
@@ -396,6 +403,60 @@ const getUserChannelProfile = asyncHandler(async(req,res) => {
   .status(200)
   .json(new ApiResponse(200,channel[0], "User channel fetched sucessfully"))
 
+})
+
+const getWatchHistory = asyncHandler(async(req,res) => {
+  const user = await User.aggregate([
+    {
+      $match:{
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup:{
+        from:"videos",
+        localField:"watchHistory",
+        foreignField:"_id",
+        as:"watchHistory",
+        pipeline:[
+          {
+            $lookup:{
+              from:"users",
+              localField:"owner",
+              foreignField:"_id",
+              as:"owner",
+              pipeline:[
+                {
+                  $project:{
+                    fullname: 1,
+                    username: 1,
+                    avatar: 1
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $addFields:{
+              owner:{
+                $first:"$owner",
+              }
+            }
+          }
+        ]
+
+
+      }
+    }
+  ])
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(
+      200, user[0].watchHistory,
+      "watch History fetched Sucessfully"
+    )
+  )
 })
 
 export { registerUser,
@@ -406,6 +467,7 @@ export { registerUser,
          getCurrentUser,
          updateAccountDetails,
          updateUserAvatar,
-         updateUserCoverImage
-
+         updateUserCoverImage,
+         getUserChannelProfile,
+         getWatchHistory,
 };
